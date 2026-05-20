0cf9a90711bd25720b158ad26a7c9c8896221291
Nguyen Pham <nguyenpham121998@gmail.com>
2026-05-20 16:32:06 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
+
+    # s = 15#helloaaaaabbbbb5#world
+    def decode(self, s: str) -> List[str]:
+        pos = 0
+        res = []
+
+        while pos < len(s) - 1:
+            # get the correct substring length (1 digit, 2 digit, etc)
+            str_len = ""
+            while s[pos] != "#":
+                str_len += s[pos]
+                pos += 1
+            
+            start = pos + 1
+            end = pos + 1 + int(str_len)
+            substr = s[start:end]
+            res.append(substr)
+            
+            pos += end
+
+        return res
+
