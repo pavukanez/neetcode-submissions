116b8d20456ec7e141f4bac49f4f4827d852b313
Nguyen Pham <nguyenpham121998@gmail.com>
2026-05-21 16:25:24 -0400

    Add: anagram-groups - submission-17

---
+++ Data Structures & Algorithms/anagram-groups/submission-17.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s:strs){
+            // sort current string
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+
+            // if key not in map yet -> map key: new List -> add current string to list
+            map.computeIfAbsent(sortedStr, k -> new ArrayList<String>()).add(s);
+        }
+
+        return new ArrayList<List<>(map.getValues())>;
+    }
+}
