37d9f9e2b37235ee8a94d470f588e83dd19de834
Nguyen Pham <nguyenpham121998@gmail.com>
2026-05-21 18:10:57 -0400

    Add: string-encode-and-decode - submission-15

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-15.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder res = new StringBuilder();
+        for (String s:strs){
+            res.append(s.length()).append("#").append(s);
+        }
+        return res.toString();
+        
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int pos = 0;
+        while (pos < str.length()){
+            int delimiter_pos = str.indexOf("#", pos);
+            int strLen = Integer.valueOf(str.substring(pos, delimiter_pos));
+
+            pos = delimiter_pos + 1;
+            res.add(str.substring(pos, pos + strLen));
+
+            pos += strLen;
+        }
+
+        return res;
+    }
+}
