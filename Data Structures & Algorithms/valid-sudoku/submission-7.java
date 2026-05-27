4d7f347fced0136d09c70ae13202311f01d8413b
Nguyen Pham <nguyenpham121998@gmail.com>
2026-05-26 20:03:45 -0400

    Add: valid-sudoku - submission-7

---
+++ Data Structures & Algorithms/valid-sudoku/submission-7.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int r=0;r<9;r++){
+            Set<Character> seen = new HashSet<>();
+            for(int c=0;c<9;c++){
+                if (board[r][c] == '.') continue;
+                if (seen.contains(board[r][c])) return false;
+                seen.add(board[r][c]);
+            }
+        }
+
+        for(int c=0;c<9;c++){
+            Set<Character> seen = new HashSet<>();
+            for(int r=0;r<9;r++){
+                if (board[r][c] == '.') continue;
+                if (seen.contains(board[r][c])) return false;
+                seen.add(board[r][c]);
+            }
+        }
+
+        Set<Character>[][] boxes = new HashSet[3][3];
+        for(int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
+                if (board[r][c] == '.') continue;
+
+                if (boxes[r/3][c/3] == null) boxes[r/3][c/3] = new HashSet<>();
+                if (boxes[r/3][c/3].contains(board[r][c])) return false;
+                boxes[r/3][c/3].add(board[r][c]);
+            }
+        }
+
+        return true;
+    }
+}
