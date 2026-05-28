efc07b8d19daf89d9ea7347df95f08ed027aabbc
Nguyen Pham <nguyenpham121998@gmail.com>
2026-05-28 15:52:04 -0400

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] countS1 = new int[26];
+        int[] countWindow = new int[26];
+
+        for (int i=0;i<s1.length();i++){
+            countS1[s1.charAt(i) - 'a']++;
+            countWindow[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i=s1.length();i<s2.length();i++){
+            if (Arrays.equals(countS1, countWindow)) return true;
+
+            countWindow[s2.charAt(i) - 'a']++;
+            countWindow[s2.charAt(i - s1.length()) - 'a']--;
+        }
+        return false;
+    }
+}
