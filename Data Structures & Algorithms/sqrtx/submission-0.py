128423de30351872537b7103b502ef6ddfb3a69c
Nguyen Pham <nguyenpham121998@gmail.com>
2026-09-14 14:29:32 -0400

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, math.floor(x / 2)
+        res = 0
+
+        while l <= r:
+            m = l + (r - l) // 2
+
+            if m * m <= x:
+                res = m
+                l = m + 1
+            else:
+                r = m - 1
+        return res
