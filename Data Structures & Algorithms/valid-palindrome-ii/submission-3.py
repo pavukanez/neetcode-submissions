88d1721a92f5c7efdb3829e9852c5cba05eda184
Nguyen Pham <nguyenpham121998@gmail.com>
2026-09-22 13:08:01 -0400

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        for i in range(len(s)):
+            temp = s[:i] + s[i+1:]
+            if temp == temp[::-1]:
+                return True
+        return False
